19c5d3ecff8c121846c05484bad70c39da5efe0f
Galushko Evgeniy <yudginchik@gmail.com>
2024-07-19 12:17:10 +0300

    add task-3 good

--- js/task-3.js
+++ js/task-3.js
@@ -1,2 +1,25 @@
 'use strict';
 
+
+const profile = {
+  players: {username: "Jacob", playTime: 300,},
+  
+  getInfo() {
+    return `${this.players.username} has ${this.players.playTime} active hours!`;
+  },
+  changeUsername(newName) {
+    this.players.username = newName;
+  },
+  updatePlayTime(hours) {
+    this.players.playTime += hours
+  }
+}
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
+
